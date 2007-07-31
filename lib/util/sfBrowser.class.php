a8301a5680bbd5a39ee7a7362642ac91045a7b37
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2007-07-31 08:46:44 +0000

    fixed typo (closes #1853)

--- lib/util/sfBrowser.class.php
+++ lib/util/sfBrowser.class.php
@@ -252,7 +252,7 @@ class sfBrowser
   {
     if (is_null($this->dom))
     {
-      throw new sfException('The DOM is not accessible because the browser response content type is not HMTL.');
+      throw new sfException('The DOM is not accessible because the browser response content type is not HTML.');
     }
 
     return $this->domCssSelector;
@@ -262,7 +262,7 @@ class sfBrowser
   {
     if (is_null($this->dom))
     {
-      throw new sfException('The DOM is not accessible because the browser response content type is not HMTL.');
+      throw new sfException('The DOM is not accessible because the browser response content type is not HTML.');
     }
 
     return $this->dom;
