50defa4041f95f17e7095ec9d10f965e9f718687
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2007-08-07 13:37:47 +0000

    fixed propel-dump-data outputs model name when table is empty (closes #1577)

--- lib/addon/propel/sfPropelData.class.php
+++ lib/addon/propel/sfPropelData.class.php
@@ -296,8 +296,6 @@ class sfPropelData extends sfData
       // get db info
       $rs = $con->executeQuery('SELECT * FROM '.constant($tableName.'Peer::TABLE_NAME'));
 
-      $dumpData[$tableName] = array();
-
       while ($rs->next())
       {
         $pk = $tableName;
@@ -319,6 +317,11 @@ class sfPropelData extends sfData
           {
             $values[$col] = $rs->get($col);
           }
+        }
+
+        if (!isset($dumpData[$tableName]))
+        {
+          $dumpData[$tableName] = array();
         }
 
         $dumpData[$tableName][$pk] = $values;
