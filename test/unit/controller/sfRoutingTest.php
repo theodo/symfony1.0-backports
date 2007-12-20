12cc2b1cdec8661f91b4da50acebfb6b468df2bd
gregoire <gregoire@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2007-12-20 17:58:30 +0000

    Fixed test number to 69

--- test/unit/controller/sfRoutingTest.php
+++ test/unit/controller/sfRoutingTest.php
@@ -10,7 +10,7 @@
 
 require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
 
-$t = new lime_test(68, new lime_output_color());
+$t = new lime_test(69, new lime_output_color());
 
 // public methods
 $r = sfRouting::getInstance();
