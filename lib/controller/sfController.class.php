1b7f0a5e13f507d3ede39ac33f942883b0a113ca
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2007-12-27 16:12:50 +0000

    fixed error message in getPresentationFor() (closes #1527)

--- lib/controller/sfController.class.php
+++ lib/controller/sfController.class.php
@@ -553,13 +553,13 @@ abstract class sfController
 
       if ($actionEntry->getModuleName() == sfConfig::get('sf_login_module') && $actionEntry->getActionName() == sfConfig::get('sf_login_action'))
       {
-        $error = 'Your mail action is secured but the user is not authenticated.';
+        $error = 'Your action is secured but the user is not authenticated.';
 
         throw new sfException($error);
       }
       else if ($actionEntry->getModuleName() == sfConfig::get('sf_secure_module') && $actionEntry->getActionName() == sfConfig::get('sf_secure_action'))
       {
-        $error = 'Your mail action is secured but the user does not have access.';
+        $error = 'Your action is secured but the user does not have access.';
 
         throw new sfException($error);
       }
