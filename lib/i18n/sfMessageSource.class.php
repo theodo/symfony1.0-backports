2bfabde4516456376dd3fb22710a13739a332902
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-05-05 08:45:37 +0000

    reverted fix for #3472 as sfMessageFormat relies on the fact ->load() always returns true (updated the API doc)

--- lib/i18n/sfMessageSource.class.php
+++ lib/i18n/sfMessageSource.class.php
@@ -150,7 +150,7 @@ abstract class sfMessageSource implements sfIMessageSource
    *  # [7] Continue with the foreach loop, e.g. goto [2].
    *
    * @param  string  a catalogue to load
-   * @return boolean true if loaded, false otherwise.
+   * @return boolean always true
    * @see    read()
    */
   function load($catalogue = 'messages')
@@ -158,7 +158,6 @@ abstract class sfMessageSource implements sfIMessageSource
     $variants = $this->getCatalogueList($catalogue);
 
     $this->messages = array();
-    $loaded = false;
 
     foreach ($variants as $variant)
     {
@@ -169,7 +168,6 @@ abstract class sfMessageSource implements sfIMessageSource
         continue;
       }
 
-      $loaded = true;
       $loadData = true;
 
       if ($this->cache)
@@ -201,7 +199,7 @@ abstract class sfMessageSource implements sfIMessageSource
       }
     }
 
-    return $loaded;
+    return true;
   }
 
   /**
