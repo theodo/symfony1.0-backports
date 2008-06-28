b34c798740aba248957549279c2da445ec4e157b
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-06-28 07:23:15 +0000

    fixed convertUrlStringToParameters breaks urlencoded parameter (closes #3788)

--- test/unit/controller/sfWebControllerTest.php
+++ test/unit/controller/sfWebControllerTest.php
@@ -11,7 +11,7 @@
 require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
 require_once($_test_dir.'/unit/sfContextMock.class.php');
 
-$t = new lime_test(17, new lime_output_color());
+$t = new lime_test(18, new lime_output_color());
 
 sfConfig::set('sf_max_forwards', 10);
 $context = new sfContext();
@@ -122,6 +122,13 @@ $tests = array(
     array(
       'id' => 12,
       'foo' => 'bar',
+    ),
+  ),
+  '@test?id=foo%26bar&foo=bar%3Dfoo' => array(
+    'test',
+    array(
+      'id' => 'foo&bar',
+      'foo' => 'bar=foo',
     ),
   ),
 );
