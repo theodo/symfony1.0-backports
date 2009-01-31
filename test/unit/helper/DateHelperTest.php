c61244b856dafe75e93a85d3e08b9896f3212e77
FabianLange <FabianLange@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-01-31 22:04:50 +0000

    [1.0] corrected test count

--- test/unit/helper/DateHelperTest.php
+++ test/unit/helper/DateHelperTest.php
@@ -12,7 +12,7 @@ require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
 
 sfLoader::loadHelpers(array('Helper', 'Asset', 'Url', 'Tag', 'Date'));
 
-$t = new lime_test(494, new lime_output_color());
+$t = new lime_test(500, new lime_output_color());
 
 class sfContext
 {
