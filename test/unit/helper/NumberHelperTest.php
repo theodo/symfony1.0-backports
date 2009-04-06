a5990f76d19ba8c6c6f8dc9406ab88964d9ab128
fabien <fabien@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-04-06 06:32:33 +0000

    [1.0, 1.1, 1.2, 1.3] added a test for format_currency (refs #5715)

--- test/unit/helper/NumberHelperTest.php
+++ test/unit/helper/NumberHelperTest.php
@@ -12,7 +12,7 @@ require_once(dirname(__FILE__).'/../../bootstrap/unit.php');
 
 sfLoader::loadHelpers(array('Helper', 'Tag', 'Number'));
 
-$t = new lime_test(2, new lime_output_color());
+$t = new lime_test(4, new lime_output_color());
 
 // format_number()
 $t->diag('format_number()');
@@ -22,3 +22,5 @@ $t->is(format_number(10012.1, 'en'), '10,012.1', 'format_number() takes a number
 $t->todo('format_number() takes the current user culture if no second argument is given');
 
 // format_currency()
+$t->is(format_currency(1200000.00, 'USD', 'en'), 'US$1,200,000.00', 'format_currency() takes a number as its first argument');
+$t->is(format_currency('1200000', 'USD', 'en'), 'US$1,200,000.00', 'format_currency() takes a number as its first argument');
